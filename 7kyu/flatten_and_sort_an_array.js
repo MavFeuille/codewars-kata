8a8ebb35060e51c1fb15628480cee1da38225030
MavFeuille <mavis.feuille@gmail.com>
2022-01-24 22:57:29 -0500

    Destructure

--- 7kyu/flatten_and_sort_an_array.js
+++ 7kyu/flatten_and_sort_an_array.js
@@ -1,6 +1,5 @@
-const flattenAndSort = (array) => {
-  return array.flat().sort((a,b) => a - b);
-}
+const flattenAndSort = (array) => array.flat().sort((a,b) => a - b);
+
 
 console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]));
 console.log(flattenAndSort([[], [1]]));
