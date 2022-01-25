bffdae3bdf1c0605895b9248045c5e35425cbd76
MavFeuille <mavis.feuille@gmail.com>
2022-01-24 22:50:48 -0500

    Add more test cases

--- 7kyu/flatten_and_sort_an_array.js
+++ 7kyu/flatten_and_sort_an_array.js
@@ -2,5 +2,6 @@ const flattenAndSort = (array) => {
   return array.flat().sort((a,b) => a - b);
 }
 
-console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]))
-console.log(flattenAndSort([[], [1]]))
+console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]));
+console.log(flattenAndSort([[], [1]]));
+console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]));
