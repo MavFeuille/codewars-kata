f07e7dffd4376455fa4aa3c4ba48c5fa8663786f
MavFeuille <mavis.feuille@gmail.com>
2022-01-25 22:00:02 -0500

    Add place for 2nd approach

--- 7kyu/flatten_and_sort_an_array.js
+++ 7kyu/flatten_and_sort_an_array.js
@@ -1,6 +1,8 @@
 // 1st approach
 const flattenAndSort = (array) => array.flat().sort((a,b) => a - b);
 
+// 2nd approach
+
 console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]));
 // console.log(flattenAndSort([[], [1]]));
 // console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]));
