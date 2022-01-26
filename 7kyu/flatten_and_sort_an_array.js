bfbae02218f641d8593e264dce599b38dcd83a59
MavFeuille <mavis.feuille@gmail.com>
2022-01-25 21:59:44 -0500

    Remove white space

--- 7kyu/flatten_and_sort_an_array.js
+++ 7kyu/flatten_and_sort_an_array.js
@@ -1,7 +1,6 @@
 // 1st approach
 const flattenAndSort = (array) => array.flat().sort((a,b) => a - b);
 
-
 console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]));
-console.log(flattenAndSort([[], [1]]));
-console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]));
+// console.log(flattenAndSort([[], [1]]));
+// console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]));
