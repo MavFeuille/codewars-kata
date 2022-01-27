f5bfcc3fc7f34159f45a03f0c50e8dde090f7c60
MavFeuille <mavis.feuille@gmail.com>
2022-01-26 22:37:32 -0500

    Create for-loop and test case

--- 8kyu/ASCII_Total.js
+++ 8kyu/ASCII_Total.js
@@ -1,4 +1,10 @@
 const uniTotal = (string) => {
   let sum = 0;
-  
+
+  for (let i = 0; i < string.length; i++) {
+    sum += string.charCodeAt(string[i]);
+    console.log("sum: ", sum);
+  }
 }
+
+console.log(uniTotal("aaa"));
