63bf86da9e6202524ece7a194cb95b37d9055675
MavFeuille <mavis.feuille@gmail.com>
2022-03-08 23:04:33 -0500

    Set up 2 for loops to traverse

--- 8kyu/reverse_a_string_with_respect_to_spaces.js
+++ 8kyu/reverse_a_string_with_respect_to_spaces.js
@@ -1,5 +1,26 @@
 const reverses = (string) => {
+  let inputArray = string.split(" ");
+  // console.log("🚀 ~ file: reverse_a_string_with_respect_to_spaces.js ~ line 3 ~ reverses ~ inputArray", inputArray)
+  let result = new Array(inputArray.length);
+  console.log("🚀 ~ file: reverse_a_string_with_respect_to_spaces.js ~ line 4 ~ reverses ~ result", result)
+  
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === " ") {
+      result[i] = " ";
+      console.log("result.length ~ Line10 : ", result.length);
+    }
+  }
 
+  let j = result.length - 1;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] !== " ") {
+      if (result[j] == " ") {
+        result[j] = inputArray[i];
+        j--;
+      }
+    }
+  }
+  console.log(result.join(""));
 }
 
 console.log(reverses("internship at g for g"));
